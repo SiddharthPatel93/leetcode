39790c6d75f29bf368ccf06d4fc98d099c363692
SiddharthPatel93 <patel93@uwm.edu>
2023-01-13 13:17:09 -0600

    passing all testcases in leetcode

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,6 +1,29 @@
-def longestCommonPrefix(self,strs):
-    count = len(strs)
-    if count ==1:
+def longestCommonPrefix(strs):
+    rstring = strs[0]
+    if len(strs) ==1:
         return strs[0]
     else:
-        return strs
+        for i in range(1,len(strs)):
+            count = 0
+            word = strs[i]
+            for char in rstring:
+                if count >= len(word):
+                    rstring =word
+                    break
+                if char == word[count]:
+                    count+=1
+                else:
+                    if count ==0:
+                        return('')
+                    newstring=rstring[0:count]
+                    rstring = newstring
+    return rstring
+
+
+
+strs = ["flower","flow","flight"]
+strs2 = ["dog","racecar","car"]
+print(longestCommonPrefix(strs))
+print(longestCommonPrefix(strs2))
+strs3=["aaa","aa","aaa"]
+print(longestCommonPrefix(strs3))
